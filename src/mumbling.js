85623ba0aa548708886e77bb2b0189ab123b86cf
undeadgrishnackh <undeadgrishnackh@gmail.com>
2019-10-06 00:50:34 +0200

    Refactor done and green

--- src/mumbling.js
+++ src/mumbling.js
@@ -1,12 +1,3 @@
 exports.mumbling = function (word) {
-  let splitted = word.split('');
-  let mumbling = '';
-  for (let index = 0; index < splitted.length; index++) {
-    const letter = splitted[index];
-    if (index > 0) {
-      mumbling = mumbling + '-';
-    }
-    mumbling = mumbling + letter.toUpperCase() + letter.toLowerCase().repeat(index);
-  }
-  return mumbling;
+  return word.split('').map((letter, index) => (letter.toUpperCase() + letter.toLowerCase().repeat(index))).join('-');
 };
