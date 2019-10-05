ef350f3827b3f6877caf02aecf158f54babcd3dc
undeadgrishnackh <undeadgrishnackh@gmail.com>
2019-10-06 00:38:15 +0200

    2 letters green

--- src/mumbling.js
+++ src/mumbling.js
@@ -1,3 +1,12 @@
 exports.mumbling = function (word) {
-  return word;
+  let splitted = word.split('');
+  let mumbling = '';
+  for (let index = 0; index < splitted.length; index++) {
+    const letter = splitted[index];
+    if (index > 0) {
+      mumbling = mumbling + '-';
+    }
+    mumbling = mumbling + letter.toUpperCase() + letter.repeat(index);
+  }
+  return mumbling;
 };
--- test/mumbling.test.js
+++ test/mumbling.test.js
@@ -3,7 +3,10 @@ const { mumbling } = require('../src/mumbling');
 
 describe('Let the cat mumbling my voice...', () => {
   it('One Letter.', () => {
-    assert.equal(mumbling('a'), 'a', 'The mumbling isn\'t as expected.');
+    assert.equal(mumbling('A'), 'A', 'The mumbling isn\'t as expected.');
+  });
+  it('Two Letters.', () => {
+    assert.equal(mumbling('Ab'), 'A-Bb', 'The mumbling isn\'t as expected.');
   });
 });
 
